371ef5667f3106416aeffb7f4008c2f70f2283ab
476554858@qq.com <zjx93021947220>
2021-09-02 21:34:04 +0800

    linkedhashmap实现lru

---
+++ src/main/java/atguigu/locksupport/LRUCacheDemo.java
@@ -0,0 +1,29 @@
+package atguigu.locksupport;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCacheDemo<K,V> extends LinkedHashMap<K,V> {
+    private int capacity;
+
+    public LRUCacheDemo(int capacity){
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return super.size() > capacity;
+    }
+
+    public static void main(String[] args) {
+        LRUCacheDemo lruCacheDemo = new LRUCacheDemo(3);
+        lruCacheDemo.put(1, "a");
+        lruCacheDemo.put(2, "b");
+        lruCacheDemo.put(3, "c");
+        lruCacheDemo.put(4, "d");
+        lruCacheDemo.put(3, "c");
+
+        System.out.println(lruCacheDemo.keySet());
+    }
+}
